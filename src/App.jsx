a75cb0f10920f52ac58a00540d62471b93505469
BrianS.dev <brianesosa@gmail.com>
2026-02-19 19:19:19 -0300

    g

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router'; // El motor de navegación
-import { ChatProvider } from './Context/ChatContext'; // Tu base de datos
-import AppRouter from './Router/AppRouter'; // Tu mapa de rutas
+import { BrowserRouter } from 'react-router';
+import { ChatProvider } from './Context/ChatContext';
+import AppRouter from './Router/AppRouter';
 
 function App() {
   return (
-    // 1. Encendemos el motor de navegación
     <BrowserRouter>
-      
-      {/* 2. Proveemos los datos de los chats a toda la app */}
       <ChatProvider>
-        
-        {/* 3. Renderizamos las pantallas según la URL */}
         <AppRouter />
-        
       </ChatProvider>
-      
     </BrowserRouter>
   )
 }
